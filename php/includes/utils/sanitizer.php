7c09b1aee2fd93efa5886ce0ceb276e1b07475bc
中原 創太 <sota-nakahara@outlook.jp>
2026-05-29 11:28:29 +0900

    サニタイザーを追記しました。

--- php/includes/utils/sanitizer.php
+++ php/includes/utils/sanitizer.php
@@ -8,7 +8,12 @@ if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
    exit;
 }
 
-
+/**
+ * XSS対策のためのエスケープ処理を再帰的に行います
+ *
+ * @param array|string $data エスケープ対象のデータ
+ * @return array|string エスケープ済みの安全なデータ
+ */
 function sanitize($data)
 {
    if (is_array($data)) {
@@ -18,19 +23,4 @@ function sanitize($data)
       //単なる文字列ならエスケープする
       return htmlspecialchars($data, ENT_QUOTES, 'UTF-8');
    }
-}
-
-function make_combined_options($start, $end, $selected_value = null)
-{
-   $options = ''; //最初に空の状態で宣言
-   for ($i = $start; $i <= $end; $i++) {
-      //%02d = 「10進数(d)で、2桁(2)に満たない場合は0で埋める」
-      $value = sprintf('%02d', $i);
-
-      //選択状態の判定（DBから取得した値と一致するかチェック）
-      $selected = ($value == $selected_value) ? ' selected' : '';
-
-      $options .= '<option value="' . $value . '"' . $selected . '>' . $value . '</option>';
-   }
-   return $options;
 }
